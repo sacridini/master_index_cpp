7d88eb3078b540fd27b83b1cf461aa8a331baffe
elacerda <tux.68000@gmail.com>
2021-02-21 15:36:06 -0300

    new version

--- main.cpp
+++ main.cpp
@@ -1,84 +1,71 @@
+// #define _CRTDBG_MAP_ALLOC
+// #include <stdlib.h>
+// #include <crtdbg.h>
 #include <iostream>
 #include <vector>
-#include <utility>
-#include <fstream>
+#include <cstdint>
+
 #include "gdal_priv.h"
 
 using namespace std;
 
-template<typename T1, typename T2>
-void get_master_index(const char *input_filepath, vector<pair<T1, T2>> &master_index, float value = 0) {
+class MasterIndex
+{
+public:
+    pair<uint64_t, uint64_t> index;
+    vector<pair<uint64_t, uint64_t>> index_array;
 
-    GDALDataset *in_ds;
-    GDALAllRegister ();
+    void show_indices()
+    {
+        for (auto iter : index_array) {
+            cout << iter.first << " " << iter.second << endl;
+        }
+    }
+};
 
-    int nrows, ncols, nbands;
+MasterIndex* get_master_index(GDALRasterBand *band, float value = 0.0f)
+{
+    MasterIndex *master_index = new MasterIndex;
+    int nrows, ncols;
     double nodata;
-    double transform[6];
 
-    in_ds = (GDALDataset *) GDALOpen (input_filepath, GA_ReadOnly);
+    nrows = band->GetYSize();
+    ncols = band->GetXSize();
+    nodata = band->GetNoDataValue();
 
-    nrows = in_ds->GetRasterBand (1)->GetYSize ();
-    ncols = in_ds->GetRasterBand (1)->GetXSize ();
-    nbands = in_ds->GetRasterCount ();
-    nodata = in_ds->GetRasterBand (1)->GetNoDataValue ();
-    in_ds->GetGeoTransform (transform);
-
-    float *input_Row = (float *) CPLMalloc (sizeof (float) * ncols);
-
-    cout << "Getting master index from file: " << *input_filepath << endl;
+    float *input_row = (float *)CPLMalloc( sizeof(float) * ncols );
 
     for (int i = 0; i < nrows; i++) {
-        in_ds->GetRasterBand (1)->RasterIO (GF_Read, 0, i, ncols, 1, input_Row, ncols, 1, GDT_Float32, 0, 0);
+        band->RasterIO(GF_Read, 0, i, ncols, 1, input_row, ncols, 1, GDT_Float32, 0, 0);
         for (int j = 0; j < ncols; j++) {
-            if (input_Row[j] != nodata && input_Row[j] > value) {
-                pair<size_t, size_t> index;
-                index.first = i;
-                index.second = j;
-                master_index.push_back(index);
+            if (input_row[j] != nodata && input_row[j] > value) {   
+                master_index->index.first = i;
+                master_index->index.second = j;
+                master_index->index_array.push_back(master_index->index);
             } else {
                 continue;
             }
         }
     }
 
-    CPLFree (input_Row);
-    GDALClose (in_ds);
-    GDALDestroyDriverManager ();
+    CPLFree(input_row);
+    return master_index;
 }
 
-template<typename T1, typename T2>
-GDALDataset subset_raster(GDALDataset &dataset, vector<pair<T1, T2>> &master_index) {
-    // TODO
-}
+int main(int argc, char **argv)
+{
+    GDALDataset *in_ds;
+    GDALAllRegister();
+    in_ds = (GDALDataset *)GDALOpen("C:\\Users\\eduardo\\Documents\\restante_2017_carbono_ton_ha_70km.tif", GA_ReadOnly);
 
-template<typename T1, typename T2>
-void save_master_index(const char *output_filepath, vector<pair<T1, T2>> &master_index) {
-    ofstream outfile;
-    outfile.open(output_filepath);
+    MasterIndex *master_index = get_master_index(in_ds->GetRasterBand(1));
+    master_index->show_indices();
 
-    for (auto iter : master_index) {
-        outfile << iter.first << " " << iter.second << endl;
-    }
+    GDALClose(in_ds);
+    GDALDestroyDriverManager();
+    delete master_index;
 
-    outfile.close();
-}
-
-template<typename T1, typename T2>
-void load_master_index(const char *input_filepath, vector<pair<T1, T2>> &master_index) {
-    // TODO
-}
-
-template<typename T1, typename T2, typename T3>
-void get_values(vector<pair<T1, T2>> &master_index, vector<T3> &values) {
-    // TODO
-}
-
-int main(int argc, char **argv) {
-    const char *input_filename = "/home/iis_backup/Documents/data/max_ndvi_ls_ts_rj_2018/max_ndvi_ls_ts_rj_2018_clip.tif";
-    const char *output_filename = "/home/iis_backup/Documents/data/max_ndvi_ls_ts_rj_2018/master_index";
-    vector<pair<size_t, size_t>> master_index;
-    get_master_index(input_filename, master_index);
-    // save_master_index(output_filename, master_index);
+    cin.get();
+    // _CrtDumpMemoryLeaks();
     return 0;
 }
